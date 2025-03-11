d7fac84be68fe7f2970447d5e3c3917d10cb631d
luiggy <luiggylml@gmail.com>
2025-03-10 22:23:38 -0500

    fix styles

--- src/components/icons/play-button/IconButton.tsx
+++ src/components/icons/play-button/IconButton.tsx
@@ -5,7 +5,7 @@ import styles from './IconButton.module.css'
 export const IconButton = (props: HTMLAttributes<SVGSVGElement>) => {
     return (
         <div className={styles.playButton}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 77 77" {...props}>
+            <svg className='svg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 77 77" {...props}>
                 <g className='g' fill="#A445ED" fillRule="evenodd">
                     <circle className='circle' cx="37.5" cy="37.5" r="37.5" opacity=".25" />
                     <path className='path' d="M29 27v21l21-10.5z" />
